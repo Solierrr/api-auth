4d9baf199d0af887839c4a850aa8d667e0dded90
Davi Silva <doasilva2009@hotmail.com>
2026-07-20 10:42:52 -0300

    refactor: remove business profile persistence from auth

--- src/main/kotlin/com/solaria/auth/repository/GeolocalizationRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.solaria.auth.repository
-
-import com.solaria.auth.entity.Geolocalization
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface GeolocalizationRepository : JpaRepository<Geolocalization, UUID>
